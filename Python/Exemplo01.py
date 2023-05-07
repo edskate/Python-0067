58befef5ae80f08c7c693a1b9609a1ded008a434
edilson gomes de lima <edskateicevida@gmail.com>
2023-05-07 20:39:59 -0300

    Criado metas

---
+++ Python/Exemplo01.py
@@ -0,0 +1,8 @@
+vendas = 3500
+
+if vendas > 4500:
+    print("Parabens bateu a 1° meta")
+elif vendas > 3000:
+    print("Parabens bateu a 2° meta")
+elif vendas > 1000:
+    print("Parabens bateu a 3° meta")
